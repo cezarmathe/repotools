aa477a7ed33aeaded01ee74ba9562ffb1f493fe1
Cezar Mathe <cezarmathe@gmail.com>
2018-12-08 22:02:23 +0200

    Build feature complete&tested (#11)

--- script_files/build_packages.sh
+++ script_files/build_packages.sh
@@ -13,10 +13,8 @@ function build_package() {
     return
   fi
 
-  source_pkg_config "${repository_name}"
-
   if [[ ! -z "${package_name}" ]]; then
-    # build a single package within a repository
+    # build a package from the specified repository
     if [[ ! -d "${PATH_REPOSITORIES}/${repository_name}/src/${package_name}" ]]; then
       echo "The repository ${repository_name} does not have a buildable package named ${package_name}."
       return
@@ -24,29 +22,29 @@ function build_package() {
       local previous_wd="$(pwd)"
 
       cd "${PATH_REPOSITORIES}/${repository_name}/src/${package_name}"
-      makechrootpkg -cur "${GLOBAL_BUILD_DIR}"
+      makechrootpkg -cur "${PATH_BUILD}"
 
       repo-add "${PATH_REPOSITORIES}/${repository_name}/${LOCAL_DB_FILE}" *.pkg.tar.xz
 
-      cp *.pkg.tar.xz "${PATH_REPOSITORIES}/${repository_name}/${LOCAL_DB_FILE}/"
+      cp *.pkg.tar.xz "${PATH_REPOSITORIES}/${repository_name}/pkg/"
 
       cd "${previous_wd}"
     fi
   else
-    # build all packages within a repository
-    echo "$(ls ${PATH_REPOSITORIES}/${repository_name}/src)" > "packages.tmp"
+    # build all packages from the specified repository
+    echo "Building all packages from ${repository_name}.."
 
     for packagename in "${PATH_REPOSITORIES}/${repository_name}/src/*"; do
-      bash "${PATH_REPOTOOLS}/repotools" -B "${packagename}"
+      bash "${PATH_REPOTOOLS}/repotools" -B "$(basename ${packagename}) ${repository_name}"
     done 
   fi
 }
 
 # Build all packages from all repositories
 function build_all_packages() {
-  echo "$(ls ${PATH_REPOSITORIES})" > "repositories.tmp" 
+  echo "Building all packages from all repositories.." 
 
   for reponame in "${PATH_REPOSITORIES}/*"; do
-      bash "${PATH_REPOTOOLS}/repotools" -b "${reponame}"
+      bash "${PATH_REPOTOOLS}/repotools" -b "$(basename ${reponame})"
   done 
 }
